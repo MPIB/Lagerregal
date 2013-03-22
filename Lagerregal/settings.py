21dd405a355e750909757b75bc3d3cc6a2683c69
Phillip Thelen <viirus@pherth.net>
2013-03-22 01:46:40 +0100

    archived devices aren't lent anymore

--- Lagerregal/settings.py
+++ Lagerregal/settings.py
@@ -84,7 +84,7 @@ SECRET_KEY = '@w_%5@z0kf8c@^=a++-awtkf^44)fk3r2qv2^m@9l+z5gm#vwo'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
