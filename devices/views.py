946071dedd1425ef93c5ad809be30ac29962c1cb
Phillip Thelen <viirus@pherth.net>
2013-07-14 17:01:27 +0200

    Only display edit history when logged in

--- devices/views.py
+++ devices/views.py
@@ -97,7 +97,8 @@ class Home(TemplateView):
         context['device_available'] = Device.objects.filter(currentlending=None).count()
         context['ipaddress_all'] = IpAddress.objects.all().count()
         context['ipaddress_available'] = IpAddress.objects.filter(device=None).count()
-        context['revisions'] = Version.objects.all().order_by("-pk")[:20]
+        if self.request.user.is_authenticated():
+            context['revisions'] = Version.objects.all().order_by("-pk")[:20]
         return context
 
 class DeviceList(ListView):
