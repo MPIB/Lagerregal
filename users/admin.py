4fb0a1a1f3b5d819fb39d567a48ba08eef0cda50
Phillip Thelen <viirus@pherth.net>
2013-07-14 19:18:47 +0200

    Switched to custom extended usermodel.

---
+++ users/admin.py
@@ -0,0 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+from models import Lageruser
+from django.contrib import admin
+
+admin.site.register(Lageruser)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Lageruser(AbstractUser):
+    
+
+    def __unicode__(self):
+        if self.first_name != "" and self.last_name != "":
+            return "{0} {1}".format(self.first_name, self.last_name)
+        else:
+            return self.username
