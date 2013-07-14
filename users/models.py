3f9df3991cb21cbbb1e390117554f6a363de3445
Phillip Thelen <viirus@pherth.net>
2013-07-14 19:59:54 +0200

    Display an indicator in the profile if devices are overdue

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class Lageruser(AbstractUser):
@@ -10,3 +11,7 @@ class Lageruser(AbstractUser):
             return "{0} {1}".format(self.first_name, self.last_name)
         else:
             return self.username
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
