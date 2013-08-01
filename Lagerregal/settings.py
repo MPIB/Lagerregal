733a2c29273752f111244d904e0bb0d67b427ea5
Phillip Thelen <viirus@pherth.net>
2013-08-01 20:24:28 +0200

    Moved Dashboardview to extra app

--- Lagerregal/settings.py
+++ Lagerregal/settings.py
@@ -132,6 +132,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+    'main',
     'devices',
     'network',
     'devicetypes',
--- devices/views.py
+++ devices/views.py
@@ -89,23 +89,6 @@ class TemplateApiDetail(generics.RetrieveUpdateDestroyAPIView):
     model = Template
     serializer_class = TemplateSerializer
 
-
-class Home(TemplateView):
-    template_name = "home.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(Home, self).get_context_data(**kwargs)
-        context['device_all'] = Device.objects.all().count()
-        context['device_available'] = Device.objects.filter(currentlending=None).count()
-        context['ipaddress_all'] = IpAddress.objects.all().count()
-        context['ipaddress_available'] = IpAddress.objects.filter(device=None).count()
-        if self.request.user.is_authenticated():
-            context['revisions'] = Version.objects.all().order_by("-pk")[:20]
-            context['newest_devices'] = Device.objects.all().order_by("-pk")[:10]
-            context["today"] = datetime.date.today()
-            context["overdue"] = Device.objects.filter(currentlending__duedate__lt = context["today"]).order_by("currentlending__duedate")
-        return context
-
 class DeviceList(ListView):
     context_object_name = 'device_list'
     paginate_by = 30
