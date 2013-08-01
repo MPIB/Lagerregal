ffc293baa04440bb6abd8388a909d447feeaf4c7
Phillip Thelen <viirus@pherth.net>
2013-08-01 20:29:09 +0200

    moved api to extra app

--- Lagerregal/urls.py
+++ Lagerregal/urls.py
@@ -4,6 +4,7 @@ from devices.views import *
 from network.views import *
 from devicetypes.views import *
 from main.views import *
+from api.views import *
 from users.views import ProfileView, UsersettingsView
 from django.views.generic import TemplateView
 from rest_framework.urlpatterns import format_suffix_patterns
