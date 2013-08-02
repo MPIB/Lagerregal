febf1f9d78035af12c68ad25513dba15bb308114
Phillip Thelen <viirus@pherth.net>
2013-08-02 17:13:37 +0200

    Device edit form now displays the extra attributes fields.

---
+++ devicetypes/ajax.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from dajax.core import Dajax
+from dajaxice.decorators import dajaxice_register
+from devices.models import Device, Room, Building, Manufacturer
+from devicetypes.models import TypeAttribute
+from django.utils import simplejson
+from django.core.urlresolvers import reverse
+
+@dajaxice_register
+def get_attributes(request, pk):
+    dajax = Dajax()
+    attributes = TypeAttribute.objects.filter(devicetype__pk = pk )
+    dajax.clear("#extra_attributes", "innerHTML")
+    for attribute in attributes:
+        item = u"""<div class="row">
+        <div class="small-3 columns">
+          <label for="right-label" class="right inline">{0}</label>
+        </div>
+        <div class="small-9 columns">
+        <input type="text" id="id_attribute_{1}" name="attribute_{1}" />
+        </div>
+      </div>
+        """.format(attribute.name, attribute.pk)
+        print item
+        dajax.append("#extra_attributes", "innerHTML", item)
+
+    return dajax.json()
