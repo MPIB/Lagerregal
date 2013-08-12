b272a8e15dc9a19eee54a8576ca8a8f282dd1c78
Phillip Thelen <viirus@pherth.net>
2013-08-12 22:59:11 +0200

    Added simple diff view for versions.

--- devices/templatetags/devicetags.py
+++ devices/templatetags/devicetags.py
@@ -10,3 +10,16 @@ def get_verbose_name(object):
 @register.simple_tag 
 def get_verbose_name_lowercase(object): 
     return object._meta.verbose_name.lower()
+
+@register.simple_tag
+def history_compare(old, new):
+    print old, new
+    if old != new:
+        if old == "" and new != "":
+            return "+ <span class='diff add'>{0}</span>".format(new)
+        elif old != "" and new == "":
+            return "<span class='diff remove'>-</span>".format(new)
+        else:
+            return "<span class='diff'>{0}</span>".format(new)
+    else:
+        return new
