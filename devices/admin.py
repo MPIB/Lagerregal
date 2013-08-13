a059aa51be698425180be2dfa3ab61a45da4fb1c
Phillip Thelen <viirus@pherth.net>
2013-08-13 02:09:52 +0200

    Should now correctly handle versioning for TypeAttributeValues.

--- devices/admin.py
+++ devices/admin.py
@@ -2,7 +2,7 @@ from django.utils.translation import ugettext_lazy as _
 from devices.models import *
 from django.contrib import admin
 import reversion
-class DeviceAdmin(reversion.VersionAdmin):
+class DeviceAdmin(admin.ModelAdmin):
 
     # List display
     list_display = ('name',)
