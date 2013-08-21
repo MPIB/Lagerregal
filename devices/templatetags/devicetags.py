f88dd4da466cac90ff182cf451c7c0d671cdfc7c
Phillip Thelen <viirus@pherth.net>
2013-08-22 00:49:59 +0200

    New Templatefilter to add css classes to variables

--- devices/templatetags/devicetags.py
+++ devices/templatetags/devicetags.py
@@ -1,6 +1,6 @@
 from django.template import Library, Node, TemplateSyntaxError
-
-
+import re
+from django.utils.safestring import mark_safe
 register = Library()
 
 @register.simple_tag 
@@ -26,3 +26,21 @@ def history_compare(old, new):
 @register.filter("is_select")
 def is_select(form_field_obj):
     return (form_field_obj.field.widget.__class__.__name__ == "Select")
+
+
+
+class_re = re.compile(r'(?<=class=["\'])(.*)(?=["\'])')
+@register.filter
+def add_class(value, css_class):
+    string = unicode(value)
+    match = class_re.search(string)
+    if match:
+        m = re.search(r'^%s$|^%s\s|\s%s\s|\s%s$' % (css_class, css_class, 
+                                                    css_class, css_class), 
+                                                    match.group(1))
+        if not m:
+            return mark_safe(class_re.sub(match.group(1) + " " + css_class, 
+                                          string))
+    else:
+        return mark_safe(string.replace('>', ' class="%s">' % css_class))
+    return value
