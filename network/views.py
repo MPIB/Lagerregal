7ba613d57711492cb17f720585f3b158acd21c35
Phillip Thelen <viirus@pherth.net>
2013-08-21 17:20:22 +0200

    IP-Address List can now be filtered for free/used

--- network/views.py
+++ network/views.py
@@ -4,6 +4,7 @@ from network.models import IpAddress
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from network.serializers import IpAddressSerializer
+from network.forms import ViewForm
 
 class IpAddressApiList(generics.ListCreateAPIView):
     model = IpAddress
@@ -14,9 +15,24 @@ class IpAddressApiDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = IpAddressSerializer
 
 class IpAddressList(ListView):
-    model = IpAddress
     context_object_name = 'ipaddress_list'
     paginate_by = 30
+
+    def get_queryset(self):
+        self.viewfilter = self.kwargs.pop("filter", "active")
+        if self.viewfilter == "all":
+            return IpAddress.objects.all()
+        elif self.viewfilter == "free":
+            return IpAddress.objects.filter(device=None)
+        elif self.viewfilter == "used":
+            return IpAddress.objects.exclude(device=None)
+        else:
+            return IpAddress.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(IpAddressList, self).get_context_data(**kwargs)
+        context["viewform"] = ViewForm(initial={'viewfilter': self.viewfilter})
+        return context
 
 class IpAddressDetail(DetailView):
     model = IpAddress
