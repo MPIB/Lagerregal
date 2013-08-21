8bcc9cb67fa90dd24521784fb9acd4c5f09b6943
Phillip Thelen <viirus@pherth.net>
2013-08-21 18:05:39 +0200

    Added Field so that users can specify a custom Pagelength

--- users/models.py
+++ users/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Lageruser(AbstractUser):
     language = models.CharField(max_length=3, null=True)
-
+    pagelength = models.IntegerField(validators=[
+            MaxValueValidator(250)
+        ], default=30)
     def __unicode__(self):
         if self.first_name != "" and self.last_name != "":
             return "{0} {1}".format(self.first_name, self.last_name)
