d1cb609038cdbc34d90eef5440454734d06cc60e
Phillip Thelen <viirus@pherth.net>
2013-08-22 17:56:40 +0200

    added descriptive help_text for pagelength formfield

--- users/forms.py
+++ users/forms.py
@@ -3,5 +3,5 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class AppearanceForm(forms.Form):
-    pagelength = forms.IntegerField(required=False)
+    pagelength = forms.IntegerField(required=False, help_text=_("The number of items displayed on one page in a list."))
 
