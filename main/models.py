a5c77b1afd2a0400c45e95d1e82acf270e7ea39d
Phillip Thelen <viirus@pherth.net>
2013-09-04 15:08:26 +0200

    New users now get a default dasboard. Fixes #41

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from users.models import Lageruser
+from django.db.models.signals import post_save
 
 widgets = {
     "edithistory": _("Edit history"),
@@ -15,3 +16,14 @@ class DashboardWidget(models.Model):
     widgetname = models.CharField(max_length=200, choices=widgets.items())
     user = models.ForeignKey(Lageruser)
     minimized = models.BooleanField(default=False)
+
+
+def create_dashboard(sender, instance, created, **kwargs):
+    if created:
+        DashboardWidget(column="l", index=0, widgetname="statistics", user=instance, minimized=False).save()
+        DashboardWidget(column="l", index=1, widgetname="newestdevices", user=instance, minimized=False).save()
+        DashboardWidget(column="l", index=2, widgetname="overdue", user=instance, minimized=False).save()
+        DashboardWidget(column="r", index=0, widgetname="edithistory", user=instance, minimized=False).save()
+    
+
+post_save.connect(create_dashboard, sender=Lageruser)
