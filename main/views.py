cb17d9d6bc088f1352b3bbcd8903ea1db3f08473
Phillip Thelen <viirus@pherth.net>
2013-12-03 00:08:06 +0100

    optimize dashboard widget loading

--- main/views.py
+++ main/views.py
@@ -19,12 +19,12 @@ def get_widget_data():
     context['ipaddress_available'] = IpAddress.objects.filter(device=None).count()
     context["ipaddress_percent"] = 100 - int((float(context["ipaddress_available"])/context["ipaddress_all"])*100)
     context["ipaddress_percentcolor"] = get_progresscolor(context["ipaddress_percent"] )
-    context['revisions'] = Version.objects.filter(content_type_id=ContentType.objects.get(model='device').id).order_by("-pk")[:20]
-    context['newest_devices'] = Device.objects.all().order_by("-pk")[:10]
+    context['revisions'] = Version.objects.select_related().filter(content_type_id=ContentType.objects.get(model='device').id).order_by("-pk")[:20]
+    context['newest_devices'] = Device.objects.select_related().all().order_by("-pk")[:10]
     context["today"] = datetime.date.today()
-    context["overdue"] = Device.objects.filter(currentlending__duedate__lt = context["today"]).order_by("currentlending__duedate")
+    context["overdue"] = Device.objects.select_related().filter(currentlending__duedate__lt = context["today"]).order_by("currentlending__duedate")
     context["groups"] = Devicegroup.objects.all()
-    context["recentlendings"] = Lending.objects.all().order_by("-pk")[:10]
+    context["recentlendings"] = Lending.objects.select_related().all().order_by("-pk")[:10]
     context['shortterm_devices'] = Device.objects.filter(templending=True)[:10]
     return context
 
