acc8c69471222821a1072072c7a68242845bd38d
Phillip Thelen <viirus@pherth.net>
2013-12-10 00:29:03 +0100

    fixed handling of typeattributes when editing device

--- devicetypes/ajax.py
+++ devicetypes/ajax.py
@@ -9,16 +9,17 @@ from django.core.urlresolvers import reverse
 @dajaxice_register
 def get_attributes(request, pk):
     dajax = Dajax()
-    attributes = TypeAttribute.objects.filter(devicetype__pk = pk )
     dajax.clear("#extra_attributes", "innerHTML")
-    for attribute in attributes:
-        item = u"""<div class="form-group">
+    if not pk == "":
+        attributes = TypeAttribute.objects.filter(devicetype__pk = pk )
+        for attribute in attributes:
+            item = u"""<div class="form-group">
     <label for="id_attribute_{1}" class="col-lg-3 control-label">{0}</label>
     <div class="col-lg-9">
         <input type="text" id="id_attribute_{1}" name="attribute_{1}" class="form-control" />
       </div>
 </div>
         """.format(attribute.name, attribute.pk)
-        dajax.append("#extra_attributes", "innerHTML", item)
+            dajax.append("#extra_attributes", "innerHTML", item)
 
     return dajax.json()
