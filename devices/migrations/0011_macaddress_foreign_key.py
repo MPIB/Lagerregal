7c6b5f4fd5cc2112dd5c52639a2d7f35132b66b7
Phillip Thelen <viirus@pherth.net>
2014-03-06 11:31:08 +0100

    not fail is the new gotofail

--- devices/migrations/0011_macaddress_foreign_key.py
+++ devices/migrations/0011_macaddress_foreign_key.py
@@ -9,7 +9,7 @@ class Migration(DataMigration):
     def forwards(self, orm):
         devices = orm.Device.objects.all()
         for device in devices:
-            if device.macaddress != None or device.macaddress != "":
+            if device.macaddress != None or not device.macaddress != "":
                 macaddress = orm.MacAddress()
                 macaddress.macaddress = device.macaddress
                 macaddress.device = device
