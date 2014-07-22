29a996cbb52b4ab6e8be7c44944061c214114966
Phillip Thelen <viirus@pherth.net>
2014-07-22 17:28:12 +0200

    add departments to admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from users.models import Lageruser
+from users.models import Lageruser, Department
 
 
 admin.site.register(Lageruser)
+admin.site.register(Department)
