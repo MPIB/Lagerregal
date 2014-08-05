5c5cf5a878cc77408e5fa716a1dc2b70bc9ce25f
Phillip Thelen <viirus@pherth.net>
2014-08-05 16:09:31 +0200

    don't delete devices when deleting departments

--- devices/models.py
+++ devices/models.py
@@ -118,7 +118,7 @@ class Device(models.Model):
     inventoried = models.DateTimeField(null=True, blank=True)
     bookmarkers = models.ManyToManyField(Lageruser, through=Bookmark, related_name="bookmarks", null=True, blank=True)
 
-    department = models.ForeignKey("users.Department", null=True, blank=True, related_name="devices")
+    department = models.ForeignKey("users.Department", null=True, blank=True, related_name="devices", on_delete=models.SET_NULL)
     is_private = models.BooleanField(default=False)
 
     def __unicode__(self):
