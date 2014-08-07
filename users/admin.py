1d8c214dc48336ac9968486d25d678d026760ff0
Phillip Thelen <viirus@pherth.net>
2014-08-07 14:00:40 +0200

    Add DepartmentUser to admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from users.models import Lageruser, Department
+from users.models import Lageruser, Department, DepartmentUser
 
 
 admin.site.register(Lageruser)
 admin.site.register(Department)
+admin.site.register(DepartmentUser)
