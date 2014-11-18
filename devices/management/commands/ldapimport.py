17b7d8882472d10d00921385fb560f5d6bc5741c
Phillip Thelen <viirus@pherth.net>
2014-11-18 16:32:47 +0100

    import departments from ldap

--- devices/management/commands/ldapimport.py
+++ devices/management/commands/ldapimport.py
@@ -3,7 +3,7 @@ import ldap
 from django.conf import settings  # import the settings file
 from django.core.management.base import BaseCommand
 import re
-from users.models import Lageruser
+from users.models import Lageruser, Department, DepartmentUser
 
 
 class Command(BaseCommand):
@@ -26,14 +26,27 @@ class Command(BaseCommand):
                     saveuser = True
                     created = True
                     user = Lageruser(username=userdata["sAMAccountName"][0])
+                    user.save()
                 for field, attr in settings.AUTH_LDAP_USER_ATTR_MAP.iteritems():
                     try:
                         new_value = userdata[attr][0].decode('unicode_escape').encode('iso8859-1').decode('utf8')
+                        if attr == settings.AUTH_LDAP_DEPARTMENT_FIELD:
+                            department_name = re.findall(settings.AUTH_LDAP_DEPARTMENT_REGEX, new_value)[-1]
+                            try:
+                                new_value = Department.objects.get(name=department_name)
+                            except Department.DoesNotExist as e:
+                                new_value = Department(name=department_name)
+                                new_value.save()
+                            print(department_name)
+                            if not new_value in user.departments.all():
+                                department_user = DepartmentUser(user=user, department=new_value, role="m")
+                                department_user.save()
                         old_value = getattr(user, field)
                         if old_value != new_value:
                             saveuser = True
                             setattr(user, field,new_value)
                     except StandardError as e:
+                        print(e)
                         if attr == "mail":
                             skipped_users += 1
                             saveuser = False
@@ -53,7 +66,7 @@ class Command(BaseCommand):
                         updated_users += 1
             print("skipped {0} users.".format(skipped_users))
             print("imported {0} new users.".format(created_users))
-            print("updated {0} exisitng users.".format(created_users))
+            print("updated {0} exisitng users.".format(updated_users))
 
         else:
             print("You have to enable the USE_LDAP setting to use the ldap import.")
