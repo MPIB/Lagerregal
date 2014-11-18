141ee3cef8e60ac33785305c00619f2981e39014
Phillip Thelen <viirus@pherth.net>
2014-11-18 16:49:13 +0100

    add support for more than one directory when importing

--- devices/management/commands/ldapimport.py
+++ devices/management/commands/ldapimport.py
@@ -12,58 +12,58 @@ class Command(BaseCommand):
             #ldap.set_option(ldap.OPT_DEBUG_LEVEL, 4095)
             l = ldap.initialize(settings.AUTH_LDAP_SERVER_URI)
             l.simple_bind_s(settings.AUTH_LDAP_BIND_DN, settings.AUTH_LDAP_BIND_PASSWORD)
-            users = l.search_ext_s(*settings.LDAP_USER_SEARCH, sizelimit=1000)
-            print("Found {0} users in LDAP directory.".format(len(users)))
             created_users = 0
             updated_users = 0
             skipped_users = 0
-            for dn, userdata in users:
-                saveuser = False
-                created = False
-                try:
-                    user = Lageruser.objects.get(username=userdata["sAMAccountName"][0])
-                except Exception as e:
-                    saveuser = True
-                    created = True
-                    user = Lageruser(username=userdata["sAMAccountName"][0])
-                    user.save()
-                for field, attr in settings.AUTH_LDAP_USER_ATTR_MAP.iteritems():
+            for directory in settings.LDAP_USER_SEARCH_DIRECTORIES:
+                users = l.search_ext_s(directory, *settings.LDAP_USER_SEARCH, sizelimit=1000)
+                print("Found {0} users in LDAP directory.".format(len(users)))
+
+                for dn, userdata in users:
+                    saveuser = False
+                    created = False
                     try:
-                        new_value = userdata[attr][0].decode('unicode_escape').encode('iso8859-1').decode('utf8')
-                        if attr == settings.AUTH_LDAP_DEPARTMENT_FIELD:
-                            department_name = re.findall(settings.AUTH_LDAP_DEPARTMENT_REGEX, new_value)[-1]
-                            try:
-                                new_value = Department.objects.get(name=department_name)
-                            except Department.DoesNotExist as e:
-                                new_value = Department(name=department_name)
-                                new_value.save()
-                            print(department_name)
-                            if not new_value in user.departments.all():
-                                department_user = DepartmentUser(user=user, department=new_value, role="m")
-                                department_user.save()
-                        old_value = getattr(user, field)
-                        if old_value != new_value:
-                            saveuser = True
-                            setattr(user, field,new_value)
-                    except StandardError as e:
-                        print(e)
-                        if attr == "mail":
-                            skipped_users += 1
-                            saveuser = False
-                            continue
-                        if attr == "sn":
+                        user = Lageruser.objects.get(username=userdata["sAMAccountName"][0])
+                    except Exception as e:
+                        saveuser = True
+                        created = True
+                        user = Lageruser(username=userdata["sAMAccountName"][0])
+                        user.save()
+                    for field, attr in settings.AUTH_LDAP_USER_ATTR_MAP.iteritems():
+                        try:
+                            new_value = userdata[attr][0].decode('unicode_escape').encode('iso8859-1').decode('utf8')
+                            if attr == settings.AUTH_LDAP_DEPARTMENT_FIELD:
+                                department_name = re.findall(settings.AUTH_LDAP_DEPARTMENT_REGEX, new_value)[-1]
+                                try:
+                                    new_value = Department.objects.get(name=department_name)
+                                except Department.DoesNotExist as e:
+                                    new_value = Department(name=department_name)
+                                    new_value.save()
+                                if not new_value in user.departments.all():
+                                    department_user = DepartmentUser(user=user, department=new_value, role="m")
+                                    department_user.save()
                             old_value = getattr(user, field)
-                            if old_value != userdata["sAMAccountName"][0]:
+                            if old_value != new_value:
                                 saveuser = True
-                                setattr(user, field, userdata["sAMAccountName"][0])
+                                setattr(user, field,new_value)
+                        except StandardError as e:
+                            if attr == "mail":
+                                skipped_users += 1
+                                saveuser = False
                                 continue
-                        print("{0} does not have a value for the attribute {1}\n\n".format(dn, attr))
-                if saveuser:
-                    user.save()
-                    if created:
-                        created_users += 1
-                    else:
-                        updated_users += 1
+                            if attr == "sn":
+                                old_value = getattr(user, field)
+                                if old_value != userdata["sAMAccountName"][0]:
+                                    saveuser = True
+                                    setattr(user, field, userdata["sAMAccountName"][0])
+                                    continue
+                            print("{0} does not have a value for the attribute {1}\n\n".format(dn, attr))
+                    if saveuser:
+                        user.save()
+                        if created:
+                            created_users += 1
+                        else:
+                            updated_users += 1
             print("skipped {0} users.".format(skipped_users))
             print("imported {0} new users.".format(created_users))
             print("updated {0} exisitng users.".format(updated_users))
