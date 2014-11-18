9615dc2e4a966c066ef75732aa4bee827758a855
Phillip Thelen <viirus@pherth.net>
2014-11-18 15:32:08 +0100

    update existing users from ldap

--- devices/management/commands/ldapimport.py
+++ devices/management/commands/ldapimport.py
@@ -2,40 +2,58 @@
 import ldap
 from django.conf import settings  # import the settings file
 from django.core.management.base import BaseCommand
-
+import re
 from users.models import Lageruser
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if settings.USE_LDAP:
+            #ldap.set_option(ldap.OPT_DEBUG_LEVEL, 4095)
             l = ldap.initialize(settings.AUTH_LDAP_SERVER_URI)
             l.simple_bind_s(settings.AUTH_LDAP_BIND_DN, settings.AUTH_LDAP_BIND_PASSWORD)
-            users = l.search_s(*settings.LDAP_USER_SEARCH)
+            users = l.search_ext_s(*settings.LDAP_USER_SEARCH, sizelimit=1000)
+            print("Found {0} users in LDAP directory.".format(len(users)))
             created_users = 0
-            for dn, user in users:
+            updated_users = 0
+            skipped_users = 0
+            for dn, userdata in users:
+                saveuser = False
+                created = False
                 try:
-                    Lageruser.objects.get(username=user["sAMAccountName"][0])
+                    user = Lageruser.objects.get(username=userdata["sAMAccountName"][0])
                 except Exception as e:
                     saveuser = True
-                    newuser = Lageruser(username=user["sAMAccountName"][0])
-                    for field, attr in settings.AUTH_LDAP_USER_ATTR_MAP.iteritems():
-                        try:
-                            setattr(newuser, field,
-                                    user[attr][0].decode('unicode_escape').encode('iso8859-1').decode('utf8'))
-                        except StandardError as e:
-                            if attr == "mail":
-                                print("No mail available. Skipping user.")
-                                saveuser = False
+                    created = True
+                    user = Lageruser(username=userdata["sAMAccountName"][0])
+                for field, attr in settings.AUTH_LDAP_USER_ATTR_MAP.iteritems():
+                    try:
+                        new_value = userdata[attr][0].decode('unicode_escape').encode('iso8859-1').decode('utf8')
+                        old_value = getattr(user, field)
+                        if old_value != new_value:
+                            saveuser = True
+                            setattr(user, field,new_value)
+                    except StandardError as e:
+                        if attr == "mail":
+                            skipped_users += 1
+                            saveuser = False
+                            continue
+                        if attr == "sn":
+                            old_value = getattr(user, field)
+                            if old_value != userdata["sAMAccountName"][0]:
+                                saveuser = True
+                                setattr(user, field, userdata["sAMAccountName"][0])
                                 continue
-                            if attr == "sn":
-                                setattr(newuser, field, user["sAMAccountName"][0])
-                                continue
-                            print("{0} does not have a value for the attribute {1}\n\n".format(dn, attr))
-                    if saveuser:
-                        newuser.save()
+                        print("{0} does not have a value for the attribute {1}\n\n".format(dn, attr))
+                if saveuser:
+                    user.save()
+                    if created:
                         created_users += 1
-            print("imported {0} users from ldap".format(created_users))
+                    else:
+                        updated_users += 1
+            print("skipped {0} users.".format(skipped_users))
+            print("imported {0} new users.".format(created_users))
+            print("updated {0} exisitng users.".format(created_users))
 
         else:
             print("You have to enable the USE_LDAP setting to use the ldap import.")
