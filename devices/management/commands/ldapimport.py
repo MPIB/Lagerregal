535c2608d4711e49c2b04b783cd1e77480e99a8d
Phillip Thelen <viirus@pherth.net>
2014-11-19 11:30:33 +0100

    paginate ldap import

--- devices/management/commands/ldapimport.py
+++ devices/management/commands/ldapimport.py
@@ -4,66 +4,110 @@ from django.conf import settings  # import the settings file
 from django.core.management.base import BaseCommand
 import re
 from users.models import Lageruser, Department, DepartmentUser
+from ldap.controls import SimplePagedResultsControl
+from ldap.ldapobject import LDAPObject
+
+class PagedResultsSearchObject(LDAPObject):
+  page_size = 50
+
+  def paged_search_ext_s(self,base,scope,filterstr='(objectClass=*)',attrlist=None,attrsonly=0,serverctrls=None,clientctrls=None,timeout=-1,sizelimit=0):
+    """
+    Behaves exactly like LDAPObject.search_ext_s() but internally uses the
+    simple paged results control to retrieve search results in chunks.
+
+    This is non-sense for really large results sets which you would like
+    to process one-by-one
+    """
+    req_ctrl = SimplePagedResultsControl(True,size=self.page_size,cookie='')
+
+    # Send first search request
+    msgid = self.search_ext(
+      *settings.LDAP_USER_SEARCH,
+      serverctrls=(serverctrls or [])+[req_ctrl]
+    )
+
+    result_pages = 0
+    all_results = []
+
+    while True:
+      rtype, rdata, rmsgid, rctrls = self.result3(msgid)
+      all_results.extend(rdata)
+      result_pages += 1
+      # Extract the simple paged results response control
+      pctrls = [
+        c
+        for c in rctrls
+        if c.controlType == SimplePagedResultsControl.controlType
+      ]
+      if pctrls:
+        if pctrls[0].cookie:
+            # Copy cookie from response control to request control
+            req_ctrl.cookie = pctrls[0].cookie
+            msgid = self.search_ext(*settings.LDAP_USER_SEARCH, serverctrls=(serverctrls or [])+[req_ctrl])
+        else:
+            break
+    return result_pages,all_results
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if settings.USE_LDAP:
             #ldap.set_option(ldap.OPT_DEBUG_LEVEL, 4095)
-            l = ldap.initialize(settings.AUTH_LDAP_SERVER_URI)
+            l = PagedResultsSearchObject(settings.AUTH_LDAP_SERVER_URI)
             l.simple_bind_s(settings.AUTH_LDAP_BIND_DN, settings.AUTH_LDAP_BIND_PASSWORD)
             created_users = 0
             updated_users = 0
             skipped_users = 0
-            for directory in settings.LDAP_USER_SEARCH_DIRECTORIES:
-                users = l.search_ext_s(directory, *settings.LDAP_USER_SEARCH, sizelimit=1000)
-                print("Found {0} users in LDAP directory.".format(len(users)))
+            page_count, users = l.paged_search_ext_s(*settings.LDAP_USER_SEARCH)
 
-                for dn, userdata in users:
-                    saveuser = False
-                    created = False
+            for dn, userdata in users:
+                saveuser = False
+                created = False
+                try:
+                    user = Lageruser.objects.get(username=userdata["sAMAccountName"][0])
+                except TypeError:
+                    continue
+                except Exception as e:
+                    saveuser = True
+                    created = True
+                    user = Lageruser(username=userdata["sAMAccountName"][0])
+                    user.save()
+                for field, attr in settings.AUTH_LDAP_USER_ATTR_MAP.iteritems():
                     try:
-                        user = Lageruser.objects.get(username=userdata["sAMAccountName"][0])
-                    except Exception as e:
-                        saveuser = True
-                        created = True
-                        user = Lageruser(username=userdata["sAMAccountName"][0])
-                        user.save()
-                    for field, attr in settings.AUTH_LDAP_USER_ATTR_MAP.iteritems():
-                        try:
-                            new_value = userdata[attr][0].decode('unicode_escape').encode('iso8859-1').decode('utf8')
-                            if attr == settings.AUTH_LDAP_DEPARTMENT_FIELD:
-                                department_name = re.findall(settings.AUTH_LDAP_DEPARTMENT_REGEX, new_value)[-1]
-                                try:
-                                    new_value = Department.objects.get(name=department_name)
-                                except Department.DoesNotExist as e:
-                                    new_value = Department(name=department_name)
-                                    new_value.save()
-                                if not new_value in user.departments.all():
-                                    department_user = DepartmentUser(user=user, department=new_value, role="m")
-                                    department_user.save()
+                        new_value = userdata[attr][0].decode('unicode_escape').encode('iso8859-1').decode('utf8')
+                        if attr == settings.AUTH_LDAP_DEPARTMENT_FIELD:
+                            department_name = re.findall(settings.AUTH_LDAP_DEPARTMENT_REGEX, new_value)[-1]
+                            try:
+                                new_value = Department.objects.get(name=department_name)
+                            except Department.DoesNotExist as e:
+                                new_value = Department(name=department_name)
+                                new_value.save()
+                            if not new_value in user.departments.all():
+                                department_user = DepartmentUser(user=user, department=new_value, role="m")
+                                department_user.save()
+                        old_value = getattr(user, field)
+                        if old_value != new_value:
+                            saveuser = True
+                            setattr(user, field,new_value)
+                    except StandardError as e:
+                        if attr == "mail" or attr == "givenName" or attr == "sn":
+                            skipped_users += 1
+                            saveuser = False
+                            continue
+                        if attr == "sn":
                             old_value = getattr(user, field)
-                            if old_value != new_value:
+                            if old_value != userdata["sAMAccountName"][0]:
                                 saveuser = True
-                                setattr(user, field,new_value)
-                        except StandardError as e:
-                            if attr == "mail":
-                                skipped_users += 1
-                                saveuser = False
+                                setattr(user, field, userdata["sAMAccountName"][0])
                                 continue
-                            if attr == "sn":
-                                old_value = getattr(user, field)
-                                if old_value != userdata["sAMAccountName"][0]:
-                                    saveuser = True
-                                    setattr(user, field, userdata["sAMAccountName"][0])
-                                    continue
-                            print("{0} does not have a value for the attribute {1}\n\n".format(dn, attr))
-                    if saveuser:
-                        user.save()
-                        if created:
-                            created_users += 1
-                        else:
-                            updated_users += 1
+                        print userdata
+                        print("{0} does not have a value for the attribute {1}\n\n".format(dn, attr))
+                if saveuser:
+                    user.save()
+                    if created:
+                        created_users += 1
+                    else:
+                        updated_users += 1
             print("skipped {0} users.".format(skipped_users))
             print("imported {0} new users.".format(created_users))
             print("updated {0} exisitng users.".format(updated_users))
