e463877c73800d8dc50cff04bc6c0faa1332f080
Phillip Thelen <viirus@pherth.net>
2014-11-19 13:51:04 +0100

    import expiration date from ldap

--- devices/management/commands/ldapimport.py
+++ devices/management/commands/ldapimport.py
@@ -6,7 +6,7 @@ import re
 from users.models import Lageruser, Department, DepartmentUser
 from ldap.controls import SimplePagedResultsControl
 from ldap.ldapobject import LDAPObject
-
+from datetime import date
 class PagedResultsSearchObject(LDAPObject):
   page_size = 50
 
@@ -71,7 +71,6 @@ class Command(BaseCommand):
                     saveuser = True
                     created = True
                     user = Lageruser(username=userdata["sAMAccountName"][0])
-                    user.save()
                 for field, attr in settings.AUTH_LDAP_USER_ATTR_MAP.iteritems():
                     try:
                         new_value = userdata[attr][0].decode('unicode_escape').encode('iso8859-1').decode('utf8')
@@ -82,35 +81,49 @@ class Command(BaseCommand):
                             except Department.DoesNotExist as e:
                                 new_value = Department(name=department_name)
                                 new_value.save()
-                            if not new_value in user.departments.all():
-                                department_user = DepartmentUser(user=user, department=new_value, role="m")
-                                department_user.save()
+                        elif attr == "accountExpires":
+                            expires_timestamp = (int(userdata["accountExpires"][0])/10000000)-11644473600
+                            new_value = date.fromtimestamp(expires_timestamp)
+
+                            if created and new_value < date.today():
+                                skipped_users += 1
+                                saveuser = False
+                                break
+
+                            if user.is_active != (new_value > date.today()):
+                                user.is_active = new_value > date.today()
+                                saveuser = True
                         old_value = getattr(user, field)
                         if old_value != new_value:
                             saveuser = True
                             setattr(user, field,new_value)
                     except StandardError as e:
-                        if attr == "mail" or attr == "givenName" or attr == "sn":
+                        if attr == "accountExpires":
+                            continue
+                        if attr == "givenName" or attr == "sn":
                             skipped_users += 1
                             saveuser = False
-                            continue
+                            break
                         if attr == "sn":
                             old_value = getattr(user, field)
                             if old_value != userdata["sAMAccountName"][0]:
                                 saveuser = True
                                 setattr(user, field, userdata["sAMAccountName"][0])
                                 continue
-                        print userdata
-                        print("{0} does not have a value for the attribute {1}\n\n".format(dn, attr))
+                        print("{0} does not have a value for the attribute {1}".format(dn, attr))
                 if saveuser:
                     user.save()
+                    if user.main_department:
+                        if not user.main_department in user.departments.all():
+                            department_user = DepartmentUser(user=user, department=user.main_department, role="m")
+                            department_user.save()
                     if created:
                         created_users += 1
                     else:
                         updated_users += 1
+
             print("skipped {0} users.".format(skipped_users))
             print("imported {0} new users.".format(created_users))
             print("updated {0} exisitng users.".format(updated_users))
-
         else:
             print("You have to enable the USE_LDAP setting to use the ldap import.")
