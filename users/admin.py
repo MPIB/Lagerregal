d214e88f11b341a71f20bfe6d90a2e90b5988b29
Michael Krause <mk@galax.is>
2016-11-23 23:35:58 +0100

    Fix: make user list in admin/ searchable

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from users.models import Lageruser, Department, DepartmentUser
 
+class LageruserAdmin(admin.ModelAdmin):
+    search_fields = ('first_name', 'last_name', )
 
-admin.site.register(Lageruser)
+admin.site.register(Lageruser, LageruserAdmin)
 admin.site.register(Department)
 admin.site.register(DepartmentUser)
