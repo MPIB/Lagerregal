215395f8347f8f6519acc492bdf41f52f0d6e673
Larissa Alice Pusch <pusch@mpib-berlin.mpg.de>
2017-10-04 12:23:29 +0200

    added model tests for devicegroups

--- devicegroups/tests.py
+++ devicegroups/tests.py
@@ -1,16 +1,22 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+import os.path
 
-Replace this with more appropriate tests for your application.
-"""
 
+from django.test.client import Client
 from django.test import TestCase
+from model_mommy import mommy
+from django.core.urlresolvers import reverse
 
+from devicegroups.models import Devicegroup
+from users.models import Lageruser
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class DevicegroupTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        my_admin = Lageruser.objects.create_superuser("test", "test@test.com", "test")
+        self.client.login(username = 'test', password = 'test')
+
+    def test_devicegroup_creation(self):
+        devicegroup = mommy.make(Devicegroup)
+        self.assertEqual(devicegroup.__unicode__(), devicegroup.name)
+        self.assertEqual(devicegroup.get_absolute_url(), reverse('devicegroup-detail', kwargs={'pk': devicegroup.pk}) )
+        self.assertEqual(devicegroup.get_edit_url(), reverse('devicegroup-edit', kwargs={'pk': devicegroup.pk}))
