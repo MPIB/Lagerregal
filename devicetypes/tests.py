f29ff1d2204733e4525556cd10067a22a2074344
Larissa Alice Pusch <pusch@mpib-berlin.mpg.de>
2017-10-09 09:24:46 +0200

    added model tests for devicetypes

--- devicetypes/tests.py
+++ devicetypes/tests.py
@@ -1,16 +1,43 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+import os.path
 
-Replace this with more appropriate tests for your application.
-"""
 
+from django.test.client import Client
 from django.test import TestCase
+from model_mommy import mommy
+from django.core.urlresolvers import reverse
 
+from devicetypes.models import Type, TypeAttribute, TypeAttributeValue
+from users.models import Lageruser
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TypeTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        myadmin = Lageruser.objects.create_superuser("test", "test@test.com", "test")
+        self.client.login(username = "test", password = "test")
+
+    def test_type_creation(self):
+        mytype = mommy.make(Type)
+        self.assertEqual(mytype.__unicode__(), mytype.name)
+        self.assertEqual(mytype.get_absolute_url(), reverse('type-detail', kwargs={'pk': mytype.pk}) )
+        self.assertEqual(mytype.get_edit_url(), reverse('type-edit', kwargs={'pk': mytype.pk}) )
+
+class TypeAttributeTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        myadmin = Lageruser.objects.create_superuser("test", "test@test.com", "test")
+        self.client.login(username = "test", password = "test")
+
+    def test_type_attribute_creation(self):
+        attr = mommy.make(TypeAttribute)
+        self.assertEqual(attr.__unicode__(), attr.name)
+        self.assertEqual(attr.get_absolute_url(), reverse('type-detail', kwargs={'pk': attr.pk}) )
+
+class TypeAttributeValueTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        myadmin = Lageruser.objects.create_superuser("test", "test@test.com", "test")
+        self.client.login(username = "test", password = "test")
+
+    def test_attribute_value_creation(self):
+        val = mommy.make(TypeAttributeValue)
+        self.assertEqual(val.__unicode__(), val.value)
