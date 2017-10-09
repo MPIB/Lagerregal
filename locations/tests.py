2d66d95f2c9f95874fb218ffdb9dffdb0cf89a38
Larissa Alice Pusch <pusch@mpib-berlin.mpg.de>
2017-10-09 09:35:40 +0200

    added model tests for locations

--- locations/tests.py
+++ locations/tests.py
@@ -1,1 +1,23 @@
-# Create your tests here.
+import os.path
+
+
+from django.test.client import Client
+from django.test import TestCase
+from model_mommy import mommy
+from django.core.urlresolvers import reverse
+
+from locations.models import Section
+from users.models import Lageruser
+
+
+class SectionTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        myadmin = Lageruser.objects.create_superuser("test", "test@test.com", "test")
+        self.client.login(username = "test", password = "test")
+
+    def test_section_creation(self):
+        section = mommy.make(Section)
+        self.assertEqual(section.__unicode__(), section.name)
+        self.assertEqual(section.get_absolute_url(), reverse('section-detail', kwargs={'pk': section.pk}) )
+        self.assertEqual(section.get_edit_url(), reverse('section-edit', kwargs={'pk': section.pk}))
