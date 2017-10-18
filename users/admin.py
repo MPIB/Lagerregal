b41140d6db949b30a031f20fda372ca1dc57a145
Michael Krause <krause@mpib-berlin.mpg.de>
2017-10-18 20:50:24 +0200

    Fix: make departmentuser searchable in Admin mode

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,9 @@ from users.models import Lageruser, Department, DepartmentUser
 class LageruserAdmin(admin.ModelAdmin):
     search_fields = ('first_name', 'last_name', )
 
+class DepartmentUserAdmin(admin.ModelAdmin):
+    search_fields = ('user__first_name', 'user__last_name', )
+
 admin.site.register(Lageruser, LageruserAdmin)
+admin.site.register(DepartmentUser, DepartmentUserAdmin)
 admin.site.register(Department)
-admin.site.register(DepartmentUser)
