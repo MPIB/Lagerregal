a02d894e2fc3f82eaeb183b5d0191790ffa3c93b
Michael Krause <krause@mpib-berlin.mpg.de>
2017-10-20 18:13:11 +0200

    Add SessionAuthentication for rest_framework

--- Lagerregal/settings_template.py
+++ Lagerregal/settings_template.py
@@ -175,6 +175,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
 
