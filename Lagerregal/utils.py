0029a1f21eed3b7f679acd88b242abd3451e1f2e
Larissa Alice Pusch <pusch@mpib-berlin.mpg.de>
2018-03-07 08:51:25 +0100

    fixes #197

--- Lagerregal/utils.py
+++ Lagerregal/utils.py
@@ -21,7 +21,7 @@ class UnicodeWriter:
     def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
         # Redirect output to a queue
         self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.writer = csv.writer(self.queue, dialect=dialect, delimiter=str(u';').encode('utf-8'), quotechar=str(u'"').encode('utf-8'))
         self.stream = f
         self.encoder = codecs.getincrementalencoder(encoding)()
 
