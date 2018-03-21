9ead87f9fbd26b4b119e3f1a4a77ebb76bbf7f7f
Larissa Alice Pusch <pusch@mpib-berlin.mpg.de>
2018-03-21 10:53:25 +0100

    checkbox at right place

--- devices/views.py
+++ devices/views.py
@@ -378,7 +378,6 @@ class DeviceCreate(CreateView):
         return context
 
     def form_valid(self, form):
-        print form.cleaned_data
         if form.cleaned_data["inventory_number_given"]:
             self.object = form.save()
             self.object.inventorynumber = "not assigned"
