4232553b65c9d9bc2a228dc2687d8d0c2f82e2e4
Larissa Alice Pusch <pusch@mpib-berlin.mpg.de>
2018-08-06 10:00:36 +0200

    edited all other necessary files

--- mail/forms.py
+++ mail/forms.py
@@ -19,20 +19,3 @@ class MailTemplateForm(forms.ModelForm):
     class Meta:
         model = MailTemplate
         fields = "__all__"
-
-
-class MailTemplateUpdateForm(MailTemplateForm):
-    def __init__(self, *args, **kwargs):
-        super(MailTemplateForm, self).__init__(*args, **kwargs)
-        if self.instance.department:
-            # a query set
-            used_db = MailTemplate.objects.values_list('usage', flat=True).filter(department=kwargs["instance"].department)
-            used = []
-            for x in used_db:
-                if x is not None:
-                    # the current templates' usage can't be excluded although it is in the used_db queryset
-                    if not self.instance.usage == x:
-                        used.append(x)
-            # all usages that are not in used
-            valid = [x for x in self.fields['usage'].choices if not any(y in x for y in used)]
-            self.fields['usage'].choices = valid
