a235f2e9a38be128bb8f9f169b22193fa66db8e5
Tobias Bengfort <bengfort@mpib-berlin.mpg.de>
2019-04-16 17:00:22 +0200

    rm unused variables
    probably copy-pasted from https://github.com/lambdalisue/django-permission/blob/master/src/permission/logics/author.py

--- devices/perms.py
+++ devices/perms.py
@@ -3,65 +3,12 @@ from __future__ import unicode_literals
 from django.contrib import auth
 from django.core.exceptions import PermissionDenied
 
-from permission.conf import settings
 from permission.logics import PermissionLogic
 
 __author__ = 'viirus'
 
 
 class DevicePermissionLogic(PermissionLogic):
-    def __init__(self,
-                 field_name=None,
-                 any_permission=None,
-                 change_permission=None,
-                 delete_permission=None):
-        """
-        Constructor
-
-        Parameters
-        ----------
-        field_name : string
-            A field name of object which store the collaborators as django
-            relational fields for django user model
-            Default value will be taken from
-            ``PERMISSION_DEFAULT_COLLABORATORS_PERMISSION_LOGIC_FIELD_NAME`` in
-            settings.
-        any_permission : boolean
-            True for give any permission of the specified object to the
-            collaborators.
-            Default value will be taken from
-            ``PERMISSION_DEFAULT_COLLABORATORS_PERMISSION_LOGIC_ANY_PERMISSION``
-            in settings.
-        change_permission : boolean
-            True for give change permission of the specified object to the
-            collaborators.
-            It will be ignored if :attr:`any_permission` is True.
-            Default value will be taken from
-            ``PERMISSION_DEFAULT_COLLABORATORS_PERMISSION_LOGIC_CHANGE_PERMISSION``
-            in settings.
-        delete_permission : boolean
-            True for give delete permission of the specified object to the
-            collaborators.
-            It will be ignored if :attr:`any_permission` is True.
-            Default value will be taken from
-            ``PERMISSION_DEFAULT_COLLABORATORS_PERMISSION_LOGIC_DELETE_PERMISSION``
-            in settings.
-        """
-        self.field_name = field_name
-        self.any_permission = any_permission
-        self.change_permission = change_permission
-        self.delete_permission = delete_permission
-
-        if self.field_name is None:
-            self.field_name = \
-                settings.PERMISSION_DEFAULT_CPL_FIELD_NAME
-        if self.any_permission is None:
-            self.any_permission = True
-        if self.change_permission is None:
-            self.change_permission = True
-        if self.delete_permission is None:
-            self.delete_permission = True
-
     def has_perm(self, user_obj, perm, obj=None):
         if not user_obj.is_authenticated:
             return False
--- network/perms.py
+++ network/perms.py
@@ -1,33 +1,11 @@
 from __future__ import unicode_literals
 
 from permission.logics import PermissionLogic
-from permission.conf import settings
 
 __author__ = 'viirus'
 
 
 class IPAddressPermissionLogic(PermissionLogic):
-    def __init__(self,
-                 field_name=None,
-                 any_permission=None,
-                 change_permission=None,
-                 delete_permission=None):
-
-        self.field_name = field_name
-        self.any_permission = any_permission
-        self.change_permission = change_permission
-        self.delete_permission = delete_permission
-
-        if self.field_name is None:
-            self.field_name = \
-                settings.PERMISSION_DEFAULT_CPL_FIELD_NAME
-        if self.any_permission is None:
-            self.any_permission = True
-        if self.change_permission is None:
-            self.change_permission = True
-        if self.delete_permission is None:
-            self.delete_permission = True
-
     def has_perm(self, user_obj, perm, obj=None):
         if not user_obj.is_authenticated:
             return False
