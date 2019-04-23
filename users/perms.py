f332237cd3c79d2a08a059531380efda8a5fbdf2
Tobias Bengfort <bengfort@mpib-berlin.mpg.de>
2019-04-23 15:40:49 +0200

    require global permissions for department admins

--- users/perms.py
+++ users/perms.py
@@ -7,7 +7,7 @@ __author__ = 'viirus'
 
 class DepartmentPermissionLogic(PermissionLogic):
     def has_perm(self, user_obj, perm, obj=None):
-        if user_obj.is_active:
+        if user_obj.is_active and user_obj.has_perm(perm):
             if obj in user_obj.departments.all():
                 department_membership = user_obj.departmentuser_set.get(department=obj)
                 if department_membership.role == "a":
