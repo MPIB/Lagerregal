076f6660cd1d6f611dbe64a0e71b0e0160bc4373
Tobias Bengfort <bengfort@mpib-berlin.mpg.de>
2019-05-07 17:29:24 +0200

    handle language activation in middleware

--- users/middleware.py
+++ users/middleware.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from django.utils import translation
 
 
 class TimezoneMiddleware:
@@ -10,4 +11,16 @@ class TimezoneMiddleware:
         if user.is_authenticated:
             if user.timezone is not None:
                 timezone.activate(user.timezone)
+        return self.get_response(request)
+
+
+class LanguageMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user
+        if user.is_authenticated:
+            if user.language is not None:
+                translation.activate(user.language)
         return self.get_response(request)
