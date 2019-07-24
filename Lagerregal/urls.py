MPIB/Lagerregal

commit d4448694965839be63934ac3a8456041da5c9b2a

rm filter urls

Lagerregal/urls.py
@@ -35,9 +35,6 @@ urlpatterns = [
     path('search/', devices_views.Search.as_view(), name="search"),
 
     path('devices/', devices_views.DeviceList.as_view(), name="device-list"),
-    path('devices/page/<int:page>/', devices_views.DeviceList.as_view(), name="device-list"),
-    path('devices/department/<department>/sorting/<sorting>/filter/<filter>/', devices_views.DeviceList.as_view(), name="device-list"),
-    path('devices/page/<int:page>/department/<department>/sorting/<sorting>/filter/<filter>/', devices_views.DeviceList.as_view(), name="device-list"),
     path('devices/add/', devices_views.DeviceCreate.as_view(), name="device-add"),
     path('devices/add/template/<int:templateid>/', devices_views.DeviceCreate.as_view(), name="device-add"),
     path('devices/add/copy/<int:copyid>/', devices_views.DeviceCreate.as_view(), name="device-add-copy"),
@@ -54,7 +51,6 @@ urlpatterns = [
     path('devices/<int:pk>/tags/', devicetags_views.DeviceTags.as_view(), name="device-tags"),
     path('devices/<int:pk>/tags/<int:tag>/', devicetags_views.DeviceTagRemove.as_view(), name="device-tag-remove"),
     path('devices/<int:pk>/lending/', devices_views.DeviceLendingList.as_view(), name="device-lending-list"),
-    path('devices/<int:pk>/lending/page/<int:page>/', devices_views.DeviceLendingList.as_view(), name="device-lending-list"),
     path('devices/<int:pk>/inventoried/', devices_views.DeviceInventoried.as_view(), name="device-inventoried"),
     path('devices/<int:pk>/bookmark/', devices_views.DeviceBookmark.as_view(), name="device-bookmark"),
     path('devices/<int:pk>/notes/create/', devices_views.NoteCreate.as_view(), name="device-note-create"),
@@ -68,27 +64,13 @@ urlpatterns = [
     path('devices/export/csv/', devices_views.ExportCsv.as_view(), name='export-csv'),
     path('devices/return/<int:lending>/', devices_views.DeviceReturn.as_view(), name="device-return"),
     path('devices/public/', xframe_options_exempt(devices_views.PublicDeviceListView.as_view()), name="public-device-list"),
-    path('devices/public/page/<int:page>/', xframe_options_exempt(devices_views.PublicDeviceListView.as_view()), name="public-device-list"),
-    path('devices/public/sorting/<sorting>/', xframe_options_exempt(devices_views.PublicDeviceListView.as_view()), name="public-device-list"),
-    path('devices/public/page/<int:page>/sorting/<sorting>/', xframe_options_exempt(devices_views.PublicDeviceListView.as_view()), name="public-device-list"),
-    path('devices/public/sorting/<sorting>/group/<group>/', xframe_options_exempt(devices_views.PublicDeviceListView.as_view()), name="public-device-list"),
-    path('devices/public/page/<int:page>/sorting/<sorting>/group/<group>/', xframe_options_exempt(devices_views.PublicDeviceListView.as_view()), name="public-device-list"),
-    path('devices/public/sorting/<sorting>/group/<group>/filter/<filter>/', xframe_options_exempt(devices_views.PublicDeviceListView.as_view()), name="public-device-list"),
-    path('devices/public/page/<int:page>/sorting/<sorting>/group/<group>/filter/<filter>/', xframe_options_exempt(devices_views.PublicDeviceListView.as_view()), name="public-device-list"),
-    path('devices/public/<int:pk>/', xframe_options_exempt(devices_views.PublicDeviceDetailView.as_view()), name="public-device-detail"),
     path('devices/templates/', devices_views.TemplateList.as_view(), name="template-list"),
-    path('devices/templates/page/<int:page>/', devices_views.TemplateList.as_view(), name="template-list"),
     path('devices/templates/add/', devices_views.TemplateCreate.as_view(), name="template-add"),
     path('devices/templates/<int:pk>/edit/', devices_views.TemplateUpdate.as_view(), name="template-edit"),
     path('devices/templates/<int:pk>/delete/', devices_views.TemplateDelete.as_view(), name="template-delete"),
 
 
     path('types/', devicetypes_views.TypeList.as_view(), name="type-list"),
-    path('types/page/<int:page>/', devicetypes_views.TypeList.as_view(), name="type-list"),
-    path('types/sorting/<sorting>/', devicetypes_views.TypeList.as_view(), name="type-list"),
-    path('types/page/<int:page>/sorting/<sorting>/', devicetypes_views.TypeList.as_view(), name="type-list"),
-    path('types/sorting/<sorting>/filter/<filter>/', devicetypes_views.TypeList.as_view(), name="type-list"),
-    path('types/page/<int:page>/sorting/<sorting>/filter/<filter>/', devicetypes_views.TypeList.as_view(), name="type-list"),
     path('types/add/', devicetypes_views.TypeCreate.as_view(), name="type-add"),
     path('types/<int:pk>/', devicetypes_views.TypeDetail.as_view(), name="type-detail"),
     path('types/<int:pk>/edit/', devicetypes_views.TypeUpdate.as_view(), name="type-edit"),
@@ -100,11 +82,6 @@ urlpatterns = [
 
 
     path('rooms/', devices_views.RoomList.as_view(), name="room-list"),
-    path('rooms/page/<int:page>/', devices_views.RoomList.as_view(), name="room-list"),
-    path('rooms/sorting/<sorting>/', devices_views.RoomList.as_view(), name="room-list"),
-    path('rooms/page/<int:page>/sorting/<sorting>/', devices_views.RoomList.as_view(), name="room-list"),
-    path('rooms/sorting/<sorting>/filter/<filter>/', devices_views.RoomList.as_view(), name="room-list"),
-    path('rooms/page/<int:page>/sorting/<sorting>/filter/<filter>/', devices_views.RoomList.as_view(), name="room-list"),
     path('rooms/add/', devices_views.RoomCreate.as_view(), name="room-add"),
     path('rooms/<int:pk>/', devices_views.RoomDetail.as_view(), name="room-detail"),
     path('rooms/<int:pk>/edit/', devices_views.RoomUpdate.as_view(), name="room-edit"),
@@ -112,11 +89,6 @@ urlpatterns = [
     path('rooms/<int:oldpk>/merge/<int:newpk>/', devices_views.RoomMerge.as_view(), name="room-merge"),
 
     path('buildings/', devices_views.BuildingList.as_view(), name="building-list"),
-    path('buildings/page/<int:page>/', devices_views.BuildingList.as_view(), name="building-list"),
-    path('buildings/sorting/<sorting>/', devices_views.BuildingList.as_view(), name="building-list"),
-    path('buildings/page/<int:page>/sorting/<sorting>/', devices_views.BuildingList.as_view(), name="building-list"),
-    path('buildings/sorting/<sorting>/filter/<filter>/', devices_views.BuildingList.as_view(), name="building-list"),
-    path('buildings/page/<int:page>/sorting/<sorting>/filter/<filter>/', devices_views.BuildingList.as_view(), name="building-list"),
     path('buildings/add/', devices_views.BuildingCreate.as_view(), name="building-add"),
     path('buildings/<int:pk>/', devices_views.BuildingDetail.as_view(), name="building-detail"),
     path('buildings/<int:pk>/edit/', devices_views.BuildingUpdate.as_view(), name="building-edit"),
@@ -124,11 +96,6 @@ urlpatterns = [
     path('buildings/<int:oldpk>/merge/<int:newpk>/', devices_views.BuildingMerge.as_view(), name="building-merge"),
 
     path('manufacturers/', devices_views.ManufacturerList.as_view(), name="manufacturer-list"),
-    path('manufacturers/page/<int:page>/', devices_views.ManufacturerList.as_view(), name="manufacturer-list"),
-    path('manufacturers/sorting/<sorting>/', devices_views.ManufacturerList.as_view(), name="manufacturer-list"),
-    path('manufacturers/page/<int:page>/sorting/<sorting>/', devices_views.ManufacturerList.as_view(), name="manufacturer-list"),
-    path('manufacturers/sorting/<sorting>/filter/<filter>/', devices_views.ManufacturerList.as_view(), name="manufacturer-list"),
-    path('manufacturers/page/<int:page>/sorting/<sorting>/filter/<filter>/', devices_views.ManufacturerList.as_view(), name="manufacturer-list"),
     path('manufacturers/add/', devices_views.ManufacturerCreate.as_view(), name="manufacturer-add"),
     path('manufacturers/<int:pk>/', devices_views.ManufacturerDetail.as_view(), name="manufacturer-detail"),
     path('manufacturers/<int:pk>/edit/', devices_views.ManufacturerUpdate.as_view(), name="manufacturer-edit"),
@@ -136,37 +103,23 @@ urlpatterns = [
     path('manufacturers/<int:oldpk>/merge/<int:newpk>/', devices_views.ManufacturerMerge.as_view(), name="manufacturer-merge"),
 
     path('mails/', mail_views.MailList.as_view(), name="mail-list"),
-    path('mails/page/<int:page>/', mail_views.MailList.as_view(), name="mail-list"),
     path('mails/add/', mail_views.MailCreate.as_view(), name="mail-add"),
     path('mails/<int:pk>/', mail_views.MailDetail.as_view(), name="mail-detail"),
     path('mails/<int:pk>/edit/', mail_views.MailUpdate.as_view(), name="mail-edit"),
     path('mails/<int:pk>/delete/', mail_views.MailDelete.as_view(), name="mail-delete"),
 
     path('devicegroups/', devicegroups_views.DevicegroupList.as_view(), name="devicegroup-list"),
-    path('devicegroups/page/<int:page>/', devicegroups_views.DevicegroupList.as_view(), name="devicegroup-list"),
-    path('devicegroups/department/<department>/sorting/<sorting>/filter/<filter>/', devicegroups_views.DevicegroupList.as_view(), name="devicegroup-list"),
-    path('devicegroups/page/<int:page>/department/<department>/sorting/<sorting>/filter/<filter>/', devicegroups_views.DevicegroupList.as_view(), name="devicegroup-list"),
     path('devicegroups/add/', devicegroups_views.DevicegroupCreate.as_view(), name="devicegroup-add"),
     path('devicegroups/<int:pk>/', devicegroups_views.DevicegroupDetail.as_view(), name="devicegroup-detail"),
     path('devicegroups/<int:pk>/edit/', devicegroups_views.DevicegroupUpdate.as_view(), name="devicegroup-edit"),
     path('devicegroups/<int:pk>/delete/', devicegroups_views.DevicegroupDelete.as_view(), name="devicegroup-delete"),
 
     path('devicetags/', devicetags_views.DevicetagList.as_view(), name="devicetag-list"),
-    path('devicetags/page/<int:page>/', devicetags_views.DevicetagList.as_view(), name="devicetag-list"),
-    path('devicetags/sorting/<sorting>/', devicetags_views.DevicetagList.as_view(), name="devicetag-list"),
-    path('devicetags/page/<int:page>/sorting/<sorting>/', devicetags_views.DevicetagList.as_view(), name="devicetag-list"),
-    path('devicetags/sorting/<sorting>/filter/<filter>/', devicetags_views.DevicetagList.as_view(), name="devicetag-list"),
-    path('devicetags/page/<int:page>/sorting/<sorting>/filter/<filter>/', devicetags_views.DevicetagList.as_view(), name="devicetag-list"),
     path('devicetags/add/', devicetags_views.DevicetagCreate.as_view(), name="devicetag-add"),
     path('devicetags/<int:pk>/edit/', devicetags_views.DevicetagUpdate.as_view(), name="devicetag-edit"),
     path('devicetags/<int:pk>/delete/', devicetags_views.DevicetagDelete.as_view(), name="devicetag-delete"),
 
     path('sections/', locations_views.SectionList.as_view(), name="section-list"),
-    path('sections/page/<int:page>/', locations_views.SectionList.as_view(), name="section-list"),
-    path('sections/sorting/<sorting>/', locations_views.SectionList.as_view(), name="section-list"),
-    path('sections/page/<int:page>/sorting/<sorting>/', locations_views.SectionList.as_view(), name="section-list"),
-    path('sections/sorting/<sorting>/filter/<filter>/', locations_views.SectionList.as_view(), name="section-list"),
-    path('sections/page/<int:page>/sorting/<sorting>/filter/<filter>/', locations_views.SectionList.as_view(), name="section-list"),
     path('sections/add/', locations_views.SectionCreate.as_view(), name="section-add"),
     path('sections/<int:pk>/', locations_views.SectionDetail.as_view(), name="section-detail"),
     path('sections/<int:pk>/edit/', locations_views.SectionUpdate.as_view(), name="section-edit"),
@@ -174,11 +127,6 @@ urlpatterns = [
     path('sections/<int:oldpk>/merge/<int:newpk>/', locations_views.SectionMerge.as_view(), name="section-merge"),
 
     path('departments/', users_views.DepartmentList.as_view(), name="department-list"),
-    path('departments/page/<int:page>/', users_views.DepartmentList.as_view(), name="department-list"),
-    path('departments/sorting/<sorting>/', users_views.DepartmentList.as_view(), name="department-list"),
-    path('departments/page/<int:page>/sorting/<sorting>/', users_views.DepartmentList.as_view(), name="department-list"),
-    path('departments/sorting/<sorting>/filter/<filter>/', users_views.DepartmentList.as_view(), name="department-list"),
-    path('departments/page/<int:page>/sorting/<sorting>/filter/<filter>/', users_views.DepartmentList.as_view(), name="department-list"),
     path('departments/add/', users_views.DepartmentCreate.as_view(), name="department-add"),
     path('departments/<int:pk>/', users_views.DepartmentDetail.as_view(), name="department-detail"),
     path('departments/<int:pk>/edit/', users_views.DepartmentUpdate.as_view(), name="department-edit"),
@@ -187,21 +135,12 @@ urlpatterns = [
     path('departments/<int:pk>/delete/', users_views.DepartmentDelete.as_view(), name="department-delete"),
 
     path('ipaddresses/', network_views.IpAddressList.as_view(), name="ipaddress-list"),
-    path('ipaddresses/department/<department>/', network_views.IpAddressList.as_view(), name="ipaddress-list"),
-    path('ipaddresses/department/<department>/filter/<filter>/', network_views.IpAddressList.as_view(), name="ipaddress-list"),
-    path('ipaddresses/department/<department>/filter/<filter>/search/<search>/', network_views.IpAddressList.as_view(), name="ipaddress-list"),
-    path('ipaddresses/page/<int:page>/', network_views.IpAddressList.as_view(), name="ipaddress-list"),
-    path('ipaddresses/page/<int:page>/department/<department>/', network_views.IpAddressList.as_view(), name="ipaddress-list"),
-    path('ipaddresses/page/<int:page>/department/<department>/filter/<filter>/', network_views.IpAddressList.as_view(), name="ipaddress-list"),
-    path('ipaddresses/page/<int:page>/department/<department>/filter/<filter>/search/<search>/', network_views.IpAddressList.as_view(), name="ipaddress-list"),
     path('ipaddresses/add/', network_views.IpAddressCreate.as_view(), name="ipaddress-add"),
     path('ipaddresses/<int:pk>/', network_views.IpAddressDetail.as_view(), name="ipaddress-detail"),
     path('ipaddresses/<int:pk>/edit/', network_views.IpAddressUpdate.as_view(), name="ipaddress-edit"),
     path('ipaddresses/<int:pk>/delete/', network_views.IpAddressDelete.as_view(), name="ipaddress-delete"),
 
     path('users/', users_views.UserList.as_view(), name="user-list"),
-    path('users/department/<department>/filter/<filter>/', users_views.UserList.as_view(), name="user-list"),
-    path('users/page/<int:page>/department/<department>/filter/<filter>/', users_views.UserList.as_view(), name="user-list"),
     path('users/<int:pk>/', users_views.ProfileView.as_view(), name="userprofile"),
     path('users/<int:pk>/ipaddress/', network_views.UserIpAddress.as_view(), name="user-ipaddress"),
     path('users/<int:pk>/ipaddress/<int:ipaddress>/', network_views.UserIpAddressRemove.as_view(), name="user-ipaddress-remove"),
@@ -209,9 +148,7 @@ urlpatterns = [
     path('settings/', login_required(users_views.UsersettingsView.as_view()), name="usersettings"),
 
     path('history/global/', history_views.Globalhistory.as_view(), name="globalhistory"),
-    path('history/global/page/<int:page>/', history_views.Globalhistory.as_view(), name="globalhistory"),
     path('history/<int:content_type_id>/<int:object_id>/', history_views.HistoryList.as_view(), name="history-list"),
-    path('history/<int:content_type_id>/<int:object_id>/page/<int:page>/', history_views.HistoryList.as_view(), name="history-list"),
     path('history/version/<int:pk>/', history_views.HistoryDetail.as_view(), name="history-detail"),
 
     path('admin/', admin.site.urls),

commit 88ee2e087048cae5a5b55a1a03f24b621814035b

fix public device list
regression from d4448694

Lagerregal/urls.py
@@ -64,6 +64,7 @@ urlpatterns = [
     path('devices/export/csv/', devices_views.ExportCsv.as_view(), name='export-csv'),
     path('devices/return/<int:lending>/', devices_views.DeviceReturn.as_view(), name="device-return"),
     path('devices/public/', xframe_options_exempt(devices_views.PublicDeviceListView.as_view()), name="public-device-list"),
+    path('devices/public/<int:pk>/', xframe_options_exempt(devices_views.PublicDeviceDetailView.as_view()), name="public-device-detail"),
     path('devices/templates/', devices_views.TemplateList.as_view(), name="template-list"),
     path('devices/templates/add/', devices_views.TemplateCreate.as_view(), name="template-add"),
     path('devices/templates/<int:pk>/edit/', devices_views.TemplateUpdate.as_view(), name="template-edit"),