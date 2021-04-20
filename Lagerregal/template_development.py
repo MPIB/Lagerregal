c53421cd6ece798a70d4bff7227e33508a3d97c4
Tobias Bengfort <bengfort@mpib-berlin.mpg.de>
2021-04-20 09:25:46 +0200

    treat "localhost" as internahl IP in development

--- Lagerregal/template_development.py
+++ Lagerregal/template_development.py
@@ -47,9 +47,7 @@ LABEL_TEMPLATES = {
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = ''
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+INTERNAL_IPS = ['127.0.0.1', 'localhost']
 
 # comment out next line to send emails to the console
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
