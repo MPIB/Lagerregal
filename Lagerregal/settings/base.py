e129912903d70408077cb698b9ed950d2e225f60
Tobias Bengfort <bengfort@mpib-berlin.mpg.de>
2021-07-20 14:50:47 +0200

    rm noty

--- Lagerregal/settings/base.py
+++ Lagerregal/settings/base.py
@@ -215,11 +215,6 @@ NPM_FILE_PATTERNS = {
         'css/dataTables.bootstrap4.min.css',
         'js/dataTables.bootstrap4.min.js',
     ],
-    'noty': [
-        'js/noty/jquery.noty.js',
-        'js/noty/layouts/*.js',
-        'js/noty/themes/*.js',
-    ],
     'popper.js': [
         'dist/umd/popper.min.js',
     ],
